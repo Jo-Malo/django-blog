576490dd0d9e95468f1b02be1fb38ab40c11f5f7
Jo Malo <juanitamalo.p@gmail.com>
2018-07-23 14:37:56 +0100

    My Django Girls app, first commit

---
+++ python.py
@@ -0,0 +1,51 @@
+# if 3 > 2:
+#     print('3 is defo greater than 2')
+# else:
+#     print('3 is not greater than 2')
+
+# name = 'Jo'
+# if name == 'Joanna':
+#     print('Hey Joanna')
+# elif name == 'Jo':
+#     print('Hey Jo')
+# else:
+#     print('Hey you')
+
+# volume = 57
+# if volume < 20:
+#     print("It's kinda quiet.")
+# elif 20 <= volume < 40:
+#     print("It's nice for background music")
+# elif 40 <= volume < 60:
+#     print("Perfect, I can hear all the details")
+# elif 60 <= volume < 80:
+#     print("Nice for parties")
+# elif 80 <= volume < 100:
+#     print("A bit loud!")
+# else:
+#     print("My ears are hurting! :(")
+
+
+
+# def hi(name):
+#     if name == 'Jo':
+#         print('Hi Jo')
+#     elif name == 'Joanna':
+#         print('Hi Joanna')
+#     else:
+#         print('Hi you')
+#
+# hi('Joe')
+
+
+
+# def hi(name):
+#     print('Hi ' + name + '!')
+#
+# girls = ['Jo', 'Alanna', 'Suley', 'Imogen']
+# for name in girls:
+#     hi(name)
+#     print('Next girl')
+
+for i in range(1, 6):
+    print(i)
