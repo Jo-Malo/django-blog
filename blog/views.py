3109e4a1d0dda42107060ea120a1425720af58f0
Jo Malo <juanitamalo.p@gmail.com>
2018-09-10 09:11:56 +0100

    new header and fonts

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+# views have the logic. they request info from the model
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
